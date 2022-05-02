ff8ff2c12851482a7d91062748b2621a5cc0caf0
Arun Jha <andjhait@gmail.com>
2022-05-02 17:15:37 +0530

    Push latest changes

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -13,6 +13,7 @@ const useStyles = makeStyles((theme) => ({
     background: theme.background,
     display: 'flex',
     paddingTop: '20px',
+    paddingBottom: '20px',
     '& a': {
       cursor: 'pointer'
     }
