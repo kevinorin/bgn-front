11851a7f487d1594454e315552a1feb701795b5e
Arun Jha <andjhait@gmail.com>
2022-05-04 01:18:00 +0530

    Push latest changes

--- src/components/FooterSocials/index.js
+++ src/components/FooterSocials/index.js
@@ -38,43 +38,43 @@ const FooterSocials = () => {
 	const classes = useStyles();
 	return (
 		<div className={classes.socialWrapper}>
-			<Link href="/" passHref>
+			{/* <Link href="/" passHref>
 				<a>
 					<img src={patron} alt="Patron" />
 				</a>
-			</Link>
-			<Link href="/">
-				<a>
+			</Link> */}
+			<Link href="https://discord.com/invite/ZtqSUmdFMJ" passHref>
+				<a target="_blank">
 					<img src={discord} alt="Discord" />
 				</a>
 			</Link>
-			<Link href="/">
+			{/* <Link href="/">
 				<a>
 					<img src={wasder} alt="Wasder" />
 				</a>
-			</Link>
-			<Link href="/">
-				<a>
+			</Link> */}
+			<Link href="https://medium.com/@blockchaingamesnetwork" passHref>
+				<a target="_blank">
 					<img src={medium} alt="Medium" />
 				</a>
 			</Link>
-			<Link href="/">
-				<a>
+			<Link href="https://www.youtube.com/channel/UCspvE_U90Doi1ox2Tpn-sLQ" passHref>
+				<a target="_blank">
 					<img src={youtube} alt="Youtube" />
 				</a>
 			</Link>
-			<Link href="/">
+			{/* <Link href="/">
 				<a>
 					<img src={theta} alt="Theta" />
 				</a>
-			</Link>
-			<Link href="/">
-				<a>
+			</Link> */}
+			<Link href="https://twitter.com/kevinorin" passHref>
+				<a target="_blank">
 					<img src={twitter} alt="Twitter" />
 				</a>
 			</Link>
-			<Link href="/">
-				<a>
+			<Link href="https://www.facebook.com/BlockchainGamesNetwork" passHref>
+				<a target="_blank">
 					<img src={facebook} alt="Facebook" />
 				</a>
 			</Link>
