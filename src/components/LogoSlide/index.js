ce54fa28c87277ba080face6786ce757fcf096a7
Arun Jha <andjhait@gmail.com>
2022-05-04 22:55:50 +0530

    Push latest changes

--- src/components/LogoSlide/index.js
+++ src/components/LogoSlide/index.js
@@ -57,14 +57,14 @@ const LogoSlide = ({ logos }) => {
     slidesToScroll: 1,
     arrows: false,
     autoplay: true,
+    autoplaySpeed: 1000,
     responsive: [
         {
           breakpoint: 1024,
           settings: {
             slidesToShow: 6,
             slidesToScroll: 3,
-            infinite: true,
-            dots: true
+            infinite: true
           }
         },
         {
@@ -72,8 +72,7 @@ const LogoSlide = ({ logos }) => {
             settings: {
               slidesToShow: 4,
               slidesToScroll: 3,
-              infinite: true,
-              dots: true
+              infinite: true
             }
           },
         {
@@ -81,9 +80,7 @@ const LogoSlide = ({ logos }) => {
           settings: {
             slidesToShow: 3,
             slidesToScroll: 1,
-            infinite: true,
-            dots: true
-
+            infinite: true
           }
         }
       ]
