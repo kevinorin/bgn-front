08cad5de3d4ae3f17eb2cec00a59f5400f961f21
Arun Jha <andjhait@gmail.com>
2022-05-04 07:58:00 +0530

    Push latest changes

--- pages/_document.js
+++ pages/_document.js
@@ -49,9 +49,20 @@ export default class MyDocument extends Document {
             type="text/css"
             href="/style/main.css"
           />
+          <script dangerouslySetInnerHTML={{
+            __html: `(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-MVZXZZ5');`}} />
           <link rel='manifest' href='/manifest.json' />
         </Head>
         <body>
+          <noscript dangerouslySetInnerHTML={{
+            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MVZXZZ5"
+        height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
+          }}
+          />
           <Main />
           <NextScript />
         </body>
