fbdc3b855771b9ccd424682c777b98b9d852fbda
Arun Jha <andjhait@gmail.com>
2022-06-25 21:34:52 +0530

    Push latest changes

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -17,7 +17,7 @@ const Seo = ({ metadata }) => {
         // Only include OG image if we have it
         // Careful: if you disable image optimization in Strapi, this will break
         ...(metadata.shareImage && {
-          images: Object?.values(metadata?.shareImage?.formats).map((image) => {
+          images: metadata?.shareImage?.formats && Object?.values(metadata?.shareImage?.formats).map((image) => {
             return {
               url: getStrapiMedia(image.url),
               width: image.width,
