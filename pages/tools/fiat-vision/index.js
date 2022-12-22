35099226e1e044647ae9bf77515c9bbb9eda8816
Arun Jha <andjhait@gmail.com>
2022-12-23 00:08:39 +0530

    push latest changes

--- pages/tools/fiat-vision/index.js
+++ pages/tools/fiat-vision/index.js
@@ -1,37 +1,46 @@
 import * as React from 'react';
-
+import { getNewsData, getGlobalData } from "../../../src/utils/api"
+import { useRouter } from "next/router"
+import { Hero } from '../../../src/components/Hero';
+import { Richtext } from '../../../src/components/Richtext';
 import Seo from '../../../src/components/Seo';
-import { getGlobalData } from '../../../src/utils/api';
 
 
-export default function Index() {
-  const metadata = {
-    metaTitle: 'Fiat Vision',
-    metaDescription: 'Subscribe, support, and have a heard voice in the development of the games you play.'
+export default function Index({ ostPrograms }) {
+  const router = useRouter()
+  if (!ostPrograms) {
+    // router.push('/404')
+    return <h1 style={{ textAlign: 'center', marginTop: '8rem', height: '50vh' }}>Page Not Found!</h1>;
   }
+  const { metadata, contentSections } = ostPrograms[0];
   return (
     <>
       <Seo metadata={metadata} />
+      {
+        contentSections && contentSections.map(function (item){
+          if (item.__component === 'sections.hero') return <Hero title={item.title} subtitle={item.smallTextWithLink} />
+          else if (item.__component === 'sections.rich-text') return <Richtext content={item.content} />
+        })
+      }
     </>
   );
 }
-  
-  export async function getStaticProps(context) {
-    const { params, locale, locales, defaultLocale, preview = null } = context
-  
-    const globalLocale = await getGlobalData(locale)
-  
-    // Fetch pages. Include drafts if preview mode is on
-  
-    if (globalLocale == null) {
-      // Giving the page no props will trigger a 404 page
-      return { props: {} }
+
+export async function getStaticProps(context) {
+  const { params } = context
+
+  // Fetch pages. Include drafts if preview mode is on
+  const ostPrograms = await getNewsData(`/pages?slug=fiat-vision`)
+
+  if (ostPrograms == null) {
+    // Giving the page no props will trigger a 404 page
+    return { props: {} }
+  }
+
+  return {
+    props: {
+      ostPrograms: ostPrograms
     }
-  
-    return {
-      props: {
-        preview,
-        global: globalLocale,
-      },
-    }
+  }
 }
+
